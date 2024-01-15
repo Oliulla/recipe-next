1ff8ac1129209a8649cb982a9fd61a7f5e356a3a
Oliulla <razasm29@gmail.com>
2024-01-15 10:43:21 +0600

    [REPLACE: DBMS MySQL to sqLite]

---
+++ prisma/migrations/20240115043929_dev1/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "validated" BOOLEAN NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
