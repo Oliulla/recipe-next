60125393a9bac15dabab1b72da5cce765e6dc8c5
Oliulla <razasm29@gmail.com>
2024-01-17 00:57:07 +0600

    changed a text

--- src/components/shared/__LastFiveRecipesInHome.jsx
+++ src/components/shared/__LastFiveRecipesInHome.jsx
@@ -1,7 +1,11 @@
-'use client'
+"use client";
 
 const LastFiveRecipesInHome = () => {
-  return <div>last five recipes added by users</div>;
+  return (
+    <div>
+      <h1 className="text-3xl mt-3 font-semibold">Latest 6 Recipes Added By Users</h1>
+    </div>
+  );
 };
 
 export default LastFiveRecipesInHome;
