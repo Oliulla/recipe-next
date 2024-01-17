43d43ec8450264eb7173bebb355cf2090661760a
Oliulla <razasm29@gmail.com>
2024-01-17 12:51:03 +0600

    [DONE]: Form validation

---
+++ src/__unused_code/__dont_touch_this_file.js
@@ -0,0 +1,324 @@
+// // "use client";
+
+// // import { FloatingLabel, Checkbox, Label, Textarea } from "flowbite-react";
+// // import { useForm, useFieldArray } from "react-hook-form";
+// // import { useEffect, useState } from "react";
+// // import BlogContentEditor from "./__RecipeInstructionsEditor";
+
+// // const AddRecipeForm = () => {
+// //   const [isLoading, setIsLoading] = useState(true);
+// //   const [ingredients, setIngredients] = useState([]);
+// //   const [inputValue, setInputValue] = useState({
+// //     instructions: "",
+// //   });
+// //   const {
+// //     handleSubmit,
+// //     control,
+// //     register,
+// //     setValue,
+// //     formState: { errors },
+// //     getValues,
+// //   } = useForm({
+// //     defaultValues: { ingredients: [] },
+// //   });
+// //   const { append, remove } = useFieldArray({
+// //     control,
+// //     name: "ingredients",
+// //   });
+
+// //   useEffect(() => {
+// //     async function fetchIngredients() {
+// //       try {
+// //         const response = await fetch("ingredients.json");
+// //         const data = await response.json();
+// //         setIngredients(data);
+// //       } catch (error) {
+// //         console.error("Error fetching ingredients:", error);
+// //       } finally {
+// //         setIsLoading(false);
+// //       }
+// //     }
+
+// //     fetchIngredients();
+// //   }, []);
+
+// //   const onSubmit = (data) => {
+// //     console.log(data);
+
+// //     const selectedArr = [];
+
+// //     data.ingredients.forEach((ingredientObj, index) => {
+// //       const ingredientName = Object.keys(ingredientObj)[0];
+// //       const isChecked = ingredientObj[ingredientName];
+
+// //       if (isChecked) {
+// //         const foundIngredient = ingredients.find(
+// //           (ing) => ing.label === ingredientName
+// //         );
+// //         if (foundIngredient) {
+// //           selectedArr.push(foundIngredient.label);
+// //         }
+// //       }
+// //     });
+
+// //     if (selectedArr.length === 0) {
+// //       alert("Select at least one ingredient");
+// //     } else {
+// //       // code will be start from here
+// //     }
+// //   };
+
+// //   const handleBlogContentEditor = (eventKey, e) => {
+// //     setInputValue((prevInputValue) => ({
+// //       ...prevInputValue,
+// //       [eventKey]: e,
+// //     }));
+// //   };
+
+// //   return (
+// //     <div className="px-4 py-2">
+// //       {isLoading ? (
+// //         <p>Loading...</p>
+// //       ) : (
+// //         <div className="bg-gray-800 min-h-screen w-full">
+// //           <h1 className="py-4 text-3xl font-bold text-center text-white">
+// //             Add A Recipe
+// //           </h1>
+// //           <form
+// //             className="w-3/4 mx-auto mt-8 pb-4"
+// //             onSubmit={handleSubmit(onSubmit)}
+// //           >
+// //             <div className="flex flex-col gap-y-8">
+// //               <div>
+// //                 <FloatingLabel
+// //                   variant="standard"
+// //                   label="Recipe Title"
+// //                   className="bg-inherit text-white"
+// //                   {...register("recipeTitle", {
+// //                     required: "Recipe title is required",
+// //                   })}
+// //                 />
+// //                 {errors.recipeTitle && (
+// //                   <p className="text-red-500">{errors.recipeTitle.message}</p>
+// //                 )}
+// //               </div>
+// //               <div>
+// //                 <Label className="text-white block mb-2">
+// //                   Recipe Ingredients
+// //                 </Label>
+// //                 <div className="grid grid-cols-7 gap-x-2 gap-y-3">
+// //                   {ingredients?.map((ingredient, index) => (
+// //                     <div
+// //                       key={ingredient.id}
+// //                       className="flex items-center gap-1"
+// //                     >
+// //                       <Checkbox
+// //                         id={ingredient.id}
+// //                         {...register(
+// //                           `ingredients.${index}.${ingredient.label}`
+// //                         )}
+// //                       />
+// //                       <Label
+// //                         htmlFor={ingredient.id}
+// //                         className="text-white text-xs"
+// //                       >
+// //                         {ingredient.label}
+// //                       </Label>
+// //                     </div>
+// //                   ))}
+// //                 </div>
+// //               </div>
+// //               <div className="max-w-md">
+// //                 <div className="mb-2 block">
+// //                   <Label
+// //                     htmlFor="instructions"
+// //                     value="Recipe Instructions"
+// //                     className="text-white"
+// //                   />
+// //                 </div>
+// //                 <Textarea
+// //                   id="instructions"
+// //                   placeholder="Leave recipe instructions here..."
+// //                   required
+// //                   rows={4}
+// //                 />
+// //               </div>
+// //               <div className="edtor">
+// //                 <BlogContentEditor
+// //                   inputValue={inputValue}
+// //                   handleInputChange={handleBlogContentEditor}
+// //                 />
+// //               </div>
+// //               <button
+// //                 type="submit"
+// //                 className="bg-blue-500 text-white p-2 rounded"
+// //               >
+// //                 Submit
+// //               </button>
+// //             </div>
+// //           </form>
+// //         </div>
+// //       )}
+// //     </div>
+// //   );
+// // };
+
+// // export default AddRecipeForm;
+
+// 'use client';
+
+// import { FloatingLabel, Checkbox, Label } from "flowbite-react";
+// import { useForm, useFieldArray, Controller } from "react-hook-form";
+// import { useEffect, useState } from "react";
+// import BlogContentEditor from "./__Editor";
+
+// const AddRecipeForm = () => {
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [ingredients, setIngredients] = useState([]);
+//   const {
+//     handleSubmit,
+//     control,
+//     register,
+//     setValue,
+//     formState: { errors },
+//     getValues,
+//   } = useForm({
+//     defaultValues: { ingredients: [] },
+//   });
+//   const { append, remove } = useFieldArray({
+//     control,
+//     name: "ingredients",
+//   });
+
+//   useEffect(() => {
+//     async function fetchIngredients() {
+//       try {
+//         const response = await fetch("ingredients.json");
+//         const data = await response.json();
+//         setIngredients(data);
+//       } catch (error) {
+//         console.error("Error fetching ingredients:", error);
+//       } finally {
+//         setIsLoading(false);
+//       }
+//     }
+
+//     fetchIngredients();
+//   }, []);
+
+//   const onSubmit = (data) => {
+//     console.log(data);
+
+//     const selectedArr = [];
+
+//     data.ingredients.forEach((ingredientObj, index) => {
+//       const ingredientName = Object.keys(ingredientObj)[0];
+//       const isChecked = ingredientObj[ingredientName];
+
+//       if (isChecked) {
+//         const foundIngredient = ingredients.find(
+//           (ing) => ing.label === ingredientName
+//         );
+//         if (foundIngredient) {
+//           selectedArr.push(foundIngredient.label);
+//         }
+//       }
+//     });
+
+//     if (selectedArr.length === 0) {
+//       alert("Select at least one ingredient");
+//     } else {
+//       // code will be start from here
+//     }
+//   };
+
+//   console.log(getValues())
+
+//   return (
+//     <div className="px-4 py-2">
+//       {isLoading ? (
+//         <p>Loading...</p>
+//       ) : (
+//         <div className="bg-gray-800 min-h-screen w-full">
+//           <h1 className="py-4 text-3xl font-bold text-center text-white">
+//             Add A Recipe
+//           </h1>
+//           <form
+//             className="w-3/4 mx-auto mt-8 pb-4"
+//             onSubmit={handleSubmit(onSubmit)}
+//           >
+//             <div className="flex flex-col gap-y-8">
+//               <div>
+//                 <FloatingLabel
+//                   variant="standard"
+//                   label="Recipe Title"
+//                   className="bg-inherit text-white"
+//                   {...register("recipeTitle", {
+//                     required: "Recipe title is required",
+//                   })}
+//                 />
+//                 {errors.recipeTitle && (
+//                   <p className="text-red-500">{errors.recipeTitle.message}</p>
+//                 )}
+//               </div>
+//               <div>
+//                 <Label className="text-white block mb-2">
+//                   Recipe Ingredients
+//                 </Label>
+//                 <div className="grid grid-cols-7 gap-x-2 gap-y-3">
+//                   {ingredients?.map((ingredient, index) => (
+//                     <div
+//                       key={ingredient.id}
+//                       className="flex items-center gap-1"
+//                     >
+//                       <Controller
+//                         render={({ field }) => (
+//                           <Checkbox id={ingredient.id} {...field} />
+//                         )}
+//                         control={control}
+//                         name={`ingredients.${index}.${ingredient.label}`}
+//                       />
+//                       <Label
+//                         htmlFor={ingredient.id}
+//                         className="text-white text-xs"
+//                       >
+//                         {ingredient.label}
+//                       </Label>
+//                     </div>
+//                   ))}
+//                 </div>
+//               </div>
+//               <div className="editor">
+//                 <div className="mb-2 block">
+//                   <Label
+//                     htmlFor="instructions"
+//                     value="Recipe Instructions"
+//                     className="text-white"
+//                   />
+//                 </div>
+//                 <Controller
+//                   render={({ field }) => (
+//                     <BlogContentEditor
+//                       inputValue={field.value}
+//                       handleInputChange={(e) => field.onChange(e)}
+//                     />
+//                   )}
+//                   control={control}
+//                   name="instructions"
+//                 />
+//               </div>
+//               <button
+//                 type="submit"
+//                 className="bg-blue-500 text-white p-2 rounded"
+//               >
+//                 Submit
+//               </button>
+//             </div>
+//           </form>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default AddRecipeForm;
--- src/components/__oneTime/__AddRecipeForm.jsx
+++ src/components/__oneTime/__AddRecipeForm.jsx
@@ -1,328 +1,3 @@
-// // "use client";
-
-// // import { FloatingLabel, Checkbox, Label, Textarea } from "flowbite-react";
-// // import { useForm, useFieldArray } from "react-hook-form";
-// // import { useEffect, useState } from "react";
-// // import BlogContentEditor from "./__RecipeInstructionsEditor";
-
-// // const AddRecipeForm = () => {
-// //   const [isLoading, setIsLoading] = useState(true);
-// //   const [ingredients, setIngredients] = useState([]);
-// //   const [inputValue, setInputValue] = useState({
-// //     instructions: "",
-// //   });
-// //   const {
-// //     handleSubmit,
-// //     control,
-// //     register,
-// //     setValue,
-// //     formState: { errors },
-// //     getValues,
-// //   } = useForm({
-// //     defaultValues: { ingredients: [] },
-// //   });
-// //   const { append, remove } = useFieldArray({
-// //     control,
-// //     name: "ingredients",
-// //   });
-
-// //   useEffect(() => {
-// //     async function fetchIngredients() {
-// //       try {
-// //         const response = await fetch("ingredients.json");
-// //         const data = await response.json();
-// //         setIngredients(data);
-// //       } catch (error) {
-// //         console.error("Error fetching ingredients:", error);
-// //       } finally {
-// //         setIsLoading(false);
-// //       }
-// //     }
-
-// //     fetchIngredients();
-// //   }, []);
-
-// //   const onSubmit = (data) => {
-// //     console.log(data);
-
-// //     const selectedArr = [];
-
-// //     data.ingredients.forEach((ingredientObj, index) => {
-// //       const ingredientName = Object.keys(ingredientObj)[0];
-// //       const isChecked = ingredientObj[ingredientName];
-
-// //       if (isChecked) {
-// //         const foundIngredient = ingredients.find(
-// //           (ing) => ing.label === ingredientName
-// //         );
-// //         if (foundIngredient) {
-// //           selectedArr.push(foundIngredient.label);
-// //         }
-// //       }
-// //     });
-
-// //     if (selectedArr.length === 0) {
-// //       alert("Select at least one ingredient");
-// //     } else {
-// //       // code will be start from here
-// //     }
-// //   };
-
-// //   const handleBlogContentEditor = (eventKey, e) => {
-// //     setInputValue((prevInputValue) => ({
-// //       ...prevInputValue,
-// //       [eventKey]: e,
-// //     }));
-// //   };
-
-// //   return (
-// //     <div className="px-4 py-2">
-// //       {isLoading ? (
-// //         <p>Loading...</p>
-// //       ) : (
-// //         <div className="bg-gray-800 min-h-screen w-full">
-// //           <h1 className="py-4 text-3xl font-bold text-center text-white">
-// //             Add A Recipe
-// //           </h1>
-// //           <form
-// //             className="w-3/4 mx-auto mt-8 pb-4"
-// //             onSubmit={handleSubmit(onSubmit)}
-// //           >
-// //             <div className="flex flex-col gap-y-8">
-// //               <div>
-// //                 <FloatingLabel
-// //                   variant="standard"
-// //                   label="Recipe Title"
-// //                   className="bg-inherit text-white"
-// //                   {...register("recipeTitle", {
-// //                     required: "Recipe title is required",
-// //                   })}
-// //                 />
-// //                 {errors.recipeTitle && (
-// //                   <p className="text-red-500">{errors.recipeTitle.message}</p>
-// //                 )}
-// //               </div>
-// //               <div>
-// //                 <Label className="text-white block mb-2">
-// //                   Recipe Ingredients
-// //                 </Label>
-// //                 <div className="grid grid-cols-7 gap-x-2 gap-y-3">
-// //                   {ingredients?.map((ingredient, index) => (
-// //                     <div
-// //                       key={ingredient.id}
-// //                       className="flex items-center gap-1"
-// //                     >
-// //                       <Checkbox
-// //                         id={ingredient.id}
-// //                         {...register(
-// //                           `ingredients.${index}.${ingredient.label}`
-// //                         )}
-// //                       />
-// //                       <Label
-// //                         htmlFor={ingredient.id}
-// //                         className="text-white text-xs"
-// //                       >
-// //                         {ingredient.label}
-// //                       </Label>
-// //                     </div>
-// //                   ))}
-// //                 </div>
-// //               </div>
-// //               <div className="max-w-md">
-// //                 <div className="mb-2 block">
-// //                   <Label
-// //                     htmlFor="instructions"
-// //                     value="Recipe Instructions"
-// //                     className="text-white"
-// //                   />
-// //                 </div>
-// //                 <Textarea
-// //                   id="instructions"
-// //                   placeholder="Leave recipe instructions here..."
-// //                   required
-// //                   rows={4}
-// //                 />
-// //               </div>
-// //               <div className="edtor">
-// //                 <BlogContentEditor
-// //                   inputValue={inputValue}
-// //                   handleInputChange={handleBlogContentEditor}
-// //                 />
-// //               </div>
-// //               <button
-// //                 type="submit"
-// //                 className="bg-blue-500 text-white p-2 rounded"
-// //               >
-// //                 Submit
-// //               </button>
-// //             </div>
-// //           </form>
-// //         </div>
-// //       )}
-// //     </div>
-// //   );
-// // };
-
-// // export default AddRecipeForm;
-
-// 'use client';
-
-// import { FloatingLabel, Checkbox, Label } from "flowbite-react";
-// import { useForm, useFieldArray, Controller } from "react-hook-form";
-// import { useEffect, useState } from "react";
-// import BlogContentEditor from "./__Editor";
-
-// const AddRecipeForm = () => {
-//   const [isLoading, setIsLoading] = useState(true);
-//   const [ingredients, setIngredients] = useState([]);
-//   const {
-//     handleSubmit,
-//     control,
-//     register,
-//     setValue,
-//     formState: { errors },
-//     getValues,
-//   } = useForm({
-//     defaultValues: { ingredients: [] },
-//   });
-//   const { append, remove } = useFieldArray({
-//     control,
-//     name: "ingredients",
-//   });
-
-//   useEffect(() => {
-//     async function fetchIngredients() {
-//       try {
-//         const response = await fetch("ingredients.json");
-//         const data = await response.json();
-//         setIngredients(data);
-//       } catch (error) {
-//         console.error("Error fetching ingredients:", error);
-//       } finally {
-//         setIsLoading(false);
-//       }
-//     }
-
-//     fetchIngredients();
-//   }, []);
-
-//   const onSubmit = (data) => {
-//     console.log(data);
-
-//     const selectedArr = [];
-
-//     data.ingredients.forEach((ingredientObj, index) => {
-//       const ingredientName = Object.keys(ingredientObj)[0];
-//       const isChecked = ingredientObj[ingredientName];
-
-//       if (isChecked) {
-//         const foundIngredient = ingredients.find(
-//           (ing) => ing.label === ingredientName
-//         );
-//         if (foundIngredient) {
-//           selectedArr.push(foundIngredient.label);
-//         }
-//       }
-//     });
-
-//     if (selectedArr.length === 0) {
-//       alert("Select at least one ingredient");
-//     } else {
-//       // code will be start from here
-//     }
-//   };
-
-//   console.log(getValues())
-
-//   return (
-//     <div className="px-4 py-2">
-//       {isLoading ? (
-//         <p>Loading...</p>
-//       ) : (
-//         <div className="bg-gray-800 min-h-screen w-full">
-//           <h1 className="py-4 text-3xl font-bold text-center text-white">
-//             Add A Recipe
-//           </h1>
-//           <form
-//             className="w-3/4 mx-auto mt-8 pb-4"
-//             onSubmit={handleSubmit(onSubmit)}
-//           >
-//             <div className="flex flex-col gap-y-8">
-//               <div>
-//                 <FloatingLabel
-//                   variant="standard"
-//                   label="Recipe Title"
-//                   className="bg-inherit text-white"
-//                   {...register("recipeTitle", {
-//                     required: "Recipe title is required",
-//                   })}
-//                 />
-//                 {errors.recipeTitle && (
-//                   <p className="text-red-500">{errors.recipeTitle.message}</p>
-//                 )}
-//               </div>
-//               <div>
-//                 <Label className="text-white block mb-2">
-//                   Recipe Ingredients
-//                 </Label>
-//                 <div className="grid grid-cols-7 gap-x-2 gap-y-3">
-//                   {ingredients?.map((ingredient, index) => (
-//                     <div
-//                       key={ingredient.id}
-//                       className="flex items-center gap-1"
-//                     >
-//                       <Controller
-//                         render={({ field }) => (
-//                           <Checkbox id={ingredient.id} {...field} />
-//                         )}
-//                         control={control}
-//                         name={`ingredients.${index}.${ingredient.label}`}
-//                       />
-//                       <Label
-//                         htmlFor={ingredient.id}
-//                         className="text-white text-xs"
-//                       >
-//                         {ingredient.label}
-//                       </Label>
-//                     </div>
-//                   ))}
-//                 </div>
-//               </div>
-//               <div className="editor">
-//                 <div className="mb-2 block">
-//                   <Label
-//                     htmlFor="instructions"
-//                     value="Recipe Instructions"
-//                     className="text-white"
-//                   />
-//                 </div>
-//                 <Controller
-//                   render={({ field }) => (
-//                     <BlogContentEditor
-//                       inputValue={field.value}
-//                       handleInputChange={(e) => field.onChange(e)}
-//                     />
-//                   )}
-//                   control={control}
-//                   name="instructions"
-//                 />
-//               </div>
-//               <button
-//                 type="submit"
-//                 className="bg-blue-500 text-white p-2 rounded"
-//               >
-//                 Submit
-//               </button>
-//             </div>
-//           </form>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default AddRecipeForm;
-
 "use client";
 
 import React, { useState, useEffect } from "react";
@@ -386,6 +61,10 @@ const AddRecipeForm = () => {
 
     if (selectedArr.length === 0) {
       alert("Select at least one ingredient");
+      return;
+    } else if (!instructions) {
+      alert("Please write your recipe instructions");
+      return;
     } else {
       // Start your code here
       console.log("Recipe Title:", recipeTitle);
@@ -409,7 +88,7 @@ const AddRecipeForm = () => {
               <div>
                 <FloatingLabel
                   variant="standard"
-                  label="Recipe Title"
+                  label="Recipe Title *"
                   className="bg-inherit focus:text-white text-white"
                   value={recipeTitle}
                   onChange={(e) => setRecipeTitle(e.target.value)}
@@ -418,7 +97,7 @@ const AddRecipeForm = () => {
               </div>
               <div>
                 <Label className="text-white block mb-2">
-                  Recipe Ingredients
+                  Recipe Ingredients *
                 </Label>
                 <div className="grid grid-cols-7 gap-x-2 gap-y-3">
                   {ingredients?.map((ingredient) => (
@@ -445,7 +124,7 @@ const AddRecipeForm = () => {
                 <div className="mb-2 block">
                   <Label
                     htmlFor="instructions"
-                    value="Recipe Instructions"
+                    value="Recipe Instructions *"
                     className="text-white"
                   />
                 </div>
@@ -456,12 +135,15 @@ const AddRecipeForm = () => {
               </div>
 
               <>
-                <div className="flex w-full items-center justify-center">
+                <div className="">
+                  <Label className="text-white block mb-2">
+                    Recipe Image{" "}
+                    <span className="text-blue-500 text-xs">(Optional)</span>
+                  </Label>
                   <Label
                     htmlFor="dropzone-file"
                     className="dark:hover:bg-bray-800 flex h-64 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600"
                   >
-                    {/* ... (your existing label content) */}
                     <div className="flex flex-col items-center justify-center pb-6 pt-5">
                       {/* Display the selected file name */}
                       {selectedFile && (
@@ -469,7 +151,6 @@ const AddRecipeForm = () => {
                           Selected File: {selectedFile.name}
                         </p>
                       )}
-                      {/* ... (your existing label content) */}
                       <svg
                         className="mb-4 h-8 w-8 text-gray-500 dark:text-gray-400"
                         aria-hidden="true"
@@ -480,7 +161,7 @@ const AddRecipeForm = () => {
                         <path
                           stroke="currentColor"
                           strokeLinecap="round"
-                          strokeLineJoin="round"
+                          strokeLinejoin="round"
                           strokeWidth="2"
                           d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
                         />
@@ -504,7 +185,7 @@ const AddRecipeForm = () => {
               </>
               <button
                 type="submit"
-                className="bg-blue-500 text-white p-2 rounded"
+                className="bg-blue-500 hover:bg-blue-600 text-white p-2 rounded"
               >
                 Submit
               </button>
