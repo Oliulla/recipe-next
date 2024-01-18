4ebe70bb00453bbbeca94ae83e43597fdf4192cc
Oliulla <razasm29@gmail.com>
2024-01-19 02:27:27 +0600

    [FIXED]: Card height

--- src/components/__oneTime/RecipeCard/__SingleCard.jsx
+++ src/components/__oneTime/RecipeCard/__SingleCard.jsx
@@ -13,8 +13,8 @@ export function SingleCard({ recipe }) {
   const dataUri = `data:image/jpeg;base64,${base64Image}`;
 
   return (
-    <Link href={`/recipe-details/${recipe.id}`}>
-      <Card className="max-w-sm" imgAlt={title} imgSrc={dataUri}>
+    <Link href={`/recipe-details/${recipe.id}`} className="block">
+      <Card className="max-w-sm h-full" imgAlt={title} imgSrc={dataUri}>
         <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           {title}
         </h5>
