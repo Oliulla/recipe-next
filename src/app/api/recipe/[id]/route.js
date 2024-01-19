1312ce91adc240dd3cf8eefe6afa426e61997b31
Oliulla <razasm29@gmail.com>
2024-01-19 19:47:33 +0600

    [DONE]: Make api for update recipe

--- src/app/api/recipe/[id]/route.js
+++ src/app/api/recipe/[id]/route.js
@@ -35,3 +35,67 @@ export async function GET(request, { params }) {
         return NextResponse.json(result, { status: statusCode },);
     }
 }
+
+
+export async function PUT(request, { params }) {
+    let result = {};
+    let statusCode = 200;
+  
+    const { id } = params;
+    console.log(id)
+  
+    try {
+      const existingRecipe = await prisma.recipe.findUnique({
+        where: {
+          id: parseInt(id),
+        },
+      });
+  
+      if (!existingRecipe) {
+        statusCode = 404;
+        result = {
+          statusCode,
+          message: "Recipe not found",
+        };
+      } else {
+        const requestBody = await request.json();
+  
+        // Extract the property to update and its value from the request body
+        const { property, value } = requestBody;
+  
+        if (!property || !value) {
+          statusCode = 400;
+          result = {
+            statusCode,
+            message: "Invalid request body",
+          };
+        } else {
+          // Update the specific property
+          const updatedRecipe = await prisma.recipe.update({
+            where: {
+              id: parseInt(id),
+            },
+            data: {
+              [property]: value,
+            },
+          });
+  
+          result = {
+            statusCode,
+            message: "Recipe updated successfully",
+            data: updatedRecipe,
+          };
+        }
+      }
+    } catch (error) {
+      statusCode = 400;
+      result = {
+        statusCode,
+        message: "API error",
+      };
+    } finally {
+      await prisma.$disconnect();
+      return NextResponse.json(result, { status: statusCode });
+    }
+  }
+  
